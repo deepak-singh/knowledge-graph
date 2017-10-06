b534b6286fbd7066a5ddacdd915e252e90fe0f33
Deepak <deepakvj92@gmail.com>
2017-10-07 01:56:38 +0530

    renaming

--- content/models.py
+++ content/models.py
@@ -26,19 +26,19 @@ class Syllabus(models.Model):
 		return  self.standard.name + '-' + self.subject.name
 
 class Content(models.Model):
-	title = models.CharField(max_length=180)
+	name = models.CharField(max_length=180)
 	body = models.TextField()
 	syllabus = models.ManyToManyField(Syllabus, related_name='contents')
 
 	def __str__(self):
-		return self.title
+		return self.name
 
 class ContentTree(MPTTModel):
-	content = models.OneToOneField(Content)
+	content = models.OneToOneField(Content, related_name='content_tree')
 	parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
 
 	class MPTTMeta:
 		order_insertion_by = []
 
 	def __str__(self):
-		return self.content.title	
+		return self.content.name	
